85cc1df1b22ffbf0a10f43e7fff12aa52668273e
Maksim Savitski <maxsavitsky2015@gmail.com>
2024-12-23 14:26:03 +0300

    chore: add progress bar to starter activity

--- app/src/main/java/by/bsu/chgkfantasyclient/StarterActivity.java
+++ app/src/main/java/by/bsu/chgkfantasyclient/StarterActivity.java
@@ -42,18 +42,7 @@ public class StarterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // for test purposes
-        /*EntityRepository.getInstance().addPlayer(new Player(1, "test1", 100, 100));
-        EntityRepository.getInstance().addPlayer(new Player(2, "test2", 500, 0));
-        EntityRepository.getInstance().addPlayer(new Player(3, "test3", 50, 100));
-        EntityRepository.getInstance().addPlayer(new Player(4, "test4", 70, 100));
-
-        EntityRepository.getInstance().addTeam(new Team(1, "team1", 10, 10));
-        EntityRepository.getInstance().addTeam(new Team(2, "team2", 110, 10));
-        EntityRepository.getInstance().addTeam(new Team(3, "team3", 0, 50));
-        EntityRepository.getInstance().addTeam(new Team(4, "team4", 0, 0));
-*/
+        setContentView(R.layout.activity_starter);
 
         if(getIntent().getBooleanExtra("log_out", false)) {
             try {
